cf8ae02b791fdcbb983226de2d362b7a1e2b904d
juham58 <59977027+juham58@users.noreply.github.com>
2020-04-06 19:28:23 -0400

    Alright baby alright, fixed it

--- No2/No2_ci.py
+++ No2/No2_ci.py
@@ -2,7 +2,7 @@ import numpy as np
 from pylab import imshow, show, plasma
 
 
-def prob_2(h, R, Z, precision_voulue):
+def prob_2_gs(h, R, Z, precision_voulue):
     # Constants
     M = R*h  # nombre de quadrillés en r
     N = Z*h  # nombre de qudrillés en h
@@ -10,7 +10,7 @@ def prob_2(h, R, Z, precision_voulue):
 
     # Création des array de potentiel
     V = np.zeros((M+1, N+1), float)
-    #Vprime = np.empty([M+1, N+1], float)
+    Vprime = np.empty([M+1, N+1], float)
 
     # Définition des conditions frontières
     V[0:1*h, :] = 150  # 1 cm fois h pour le cylindre du centre
@@ -20,7 +20,8 @@ def prob_2(h, R, Z, precision_voulue):
     # Main loop
     delta = 1.0
     compteur = 0
-    Vprime = V
+    Vprime[:] = V
+    print("h: ", h, "M: ", M)
     while delta > precision_voulue:
         compteur += 1
 
@@ -32,14 +33,15 @@ def prob_2(h, R, Z, precision_voulue):
                     Vprime[i, j] = Vprime[i, j]
 
                 else:
-                    Vprime[i, j] = 1/4*(Vprime[i+1, j] + Vprime[i-1, j] + Vprime[i, j+1] + Vprime[i, j-1])
+                    Vprime[i, j] = 1/4*(h/(2*i*h)*(Vprime[i+1, j]-Vprime[i-1, j])
+                        + Vprime[i+1, j] + Vprime[i-1, j] + Vprime[i, j+1] + Vprime[i, j-1])
 
         # Calcul le max de différence entre nouvelles et vieilles valeurs
         delta = np.max(abs(V-Vprime))
         print("compteur: ", compteur, "delta: ", delta)
 
         # On échange les deux array pour recommencer
-        V = Vprime
+        V[:] = Vprime[:]
 
     # Make a plot
     imshow(Vprime)
@@ -47,4 +49,4 @@ def prob_2(h, R, Z, precision_voulue):
     show()
 
 
-prob_2(10, 10, 30, 1e-1)
+prob_2_gs(10, 10, 30, 1e-2)
